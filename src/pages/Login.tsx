9e1069ac71894a5bd20dc015ab9716ae75e9575a
PratishBuzzra <bajracharyapratish148@gmail.com>
2026-02-23 16:10:25 +0545

    password hide and show

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,10 @@
 
+import { useState } from "react";
+import { RiEyeCloseFill } from "react-icons/ri";
+import { RxEyeOpen } from "react-icons/rx";
 
 const Login = () => {
+    const [showPass, setShowPass] = useState<boolean>(false)
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
        <div className="bg-white shadow-2xl rounded-lg p-8 w-full max-w-md">
@@ -10,9 +14,10 @@ const Login = () => {
                 <label className="block text-lg font-medium" htmlFor="email">Email</label>
                 <input type="email" id="email" placeholder="Enter Your Email" className="mt-1 w-full border border-gray-500 px-3 py-2 rounded"/>
             </div>
-              <div>
+              <div className="relative">
                 <label className="block text-lg font-medium" htmlFor="password">Password</label>
-                <input type="password" id="password" placeholder="Enter Your Password" className="mt-1 w-full border border-gray-500 px-3 py-2 rounded"/>
+                <input type={showPass ? "text" : "password"} id="password" placeholder="Enter Your Password" className="mt-1 w-full border border-gray-500 px-3 py-2 rounded"/>
+                <button type="button" onClick={()=>setShowPass(prev=>!prev)} className="absolute right-3 top-13 transform -translate-y-1/2">{showPass ? <RxEyeOpen size={20}/> : <RiEyeCloseFill size={20}/>}</button>
             </div>
             <button className="w-full bg-black text-white py-2 mt-3 rounded hover:bg-gray-500">Login</button>
         </form>
